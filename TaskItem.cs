71dac898bf0e3206233e561c4a043b294238ecc3
ST10472944 <ST10472944@rcconnect.edu.za>
2026-06-25 21:47:32 +0200

    Add files via upload

---
+++ TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberAwarenessChatbotGUI
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ReminderDate { get; set; }  // store as string for display
+        public bool IsCompleted { get; set; }
+    }
+}
