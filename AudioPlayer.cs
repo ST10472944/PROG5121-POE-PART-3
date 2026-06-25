170ce0cb81a5b2968e2279aa2406687e9fc95902
ST10472944 <ST10472944@rcconnect.edu.za>
2026-06-25 21:38:49 +0200

    Add files via upload

---
+++ AudioPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Media;
+using System.IO;  
+
+namespace CyberAwarenessChatbotGUI
+{
+    public static class AudioPlayer
+    {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "greeting.wav");
+                if (File.Exists(path))
+                {
+                    using (SoundPlayer player = new SoundPlayer(path))
+                    {
+                        player.PlaySync();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Silent fail - program continues
+            }
+        }
+    }
+}
